eca159a60bda9e19cf7169991a97926117a7dcea
Ganapathy Karthikeyan <rganapathykarthikeyan8@gmail.com>
2024-02-22 15:45:43 +0530

    Pages Created

---
+++ app/community/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Community Page</div>
+  )
+}
+
+export default Page
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      <Link href='/meals'>Meals</Link>
+      <Link href='/community'>Meals</Link>
     </main>
   );
 }
